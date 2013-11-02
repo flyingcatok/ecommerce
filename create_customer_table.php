0590c6c2f92310d9b1130c0738c07801168bbf79
lafandahalf <Libby@Libbys-MacBook-Air.local>
2013-11-02 16:03:03 -0400

    file to create customer table

---
+++ create_customer_table.php
@@ -0,0 +1,40 @@
+//Author: Libby Ferland
+//Date: 11/2/2013
+<?php
+
+//create table
+$createString = "CREATE TABLE Customer
+    (Email TEXT NOT NULL, PRIMARY KEY(Email),
+     Password TEXT NOT NULL,
+     Fname TEXT NOT NULL,
+     Lname TEXT NOT NULL,
+     AddLine1 TEXT NOT NULL,
+     AddLine2 TEXT,
+     City TEXT NOT NULL,
+     State CHAR(2) NOT NULL,
+     Zip CHAR(5) NOT NULL,
+     CardNo BIGINT NOT NULL,
+     ExpirDate TEXT NOT NULL,
+     CHolderName TEXT NOT NULL,
+     BillingAddr1 TEXT NOT NULL,
+     BillingAddr2 TEXT,
+     BillingCity TEXT NOT NULL,
+     BillingState CHAR(2) NOT NULL,
+     BillingZip INT NOT NULL,
+     IsVIP TINYINT(1) NOT NULL)
+     ENGINE = INNODB";
+
+//check to make sure table is there
+
+if (mysqli_query($con, $createString))
+{
+    echo "Table Customer created successfully";
+}
+else 
+{
+    echo "Error creating table Customer: " . mysqli_error($con);
+}
+
+?>
+
+     
