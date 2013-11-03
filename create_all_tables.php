c386218af1036e1cc3a7654572f8a8b29dd25e80
Feiyu Shi <flyingcatok@gmail.com>
2013-11-02 23:07:20 -0400

    1. move author and date into php domain.\n 2. create a php code to include all the tables. \n 3. add a test code for creating tables. \n 4. correct some errors. but there is still errors in order table. need to fix that.

---
+++ create_all_tables.php
@@ -0,0 +1,15 @@
+<?php
+
+/* list all tables here */
+
+// Entities
+include 'create_customer_table.php';
+include 'create_basket_table.php';// need to stay after customer tables is created.
+include 'create_employee_table.php';
+include 'create_item_table.php';
+include 'create_order_table.php';
+include 'create_vip_table.php';
+
+// Realationships
+
+?>
