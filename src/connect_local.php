e46ff209dacdba5f7653a902b337cd94383d61fe
Feiyu Shi <flyingcatok@gmail.com>
2013-11-08 23:22:50 -0500

    arrange the layout of the page. add link to login and register

--- src/connect_local.php
+++ src/connect_local.php
@@ -3,15 +3,15 @@
 $usr = "cs405";
 $pin = "cs405";
 // global $con;
-$con = mysqli_connect('localhost:8889', $usr, $pin, 'cs405');
+$con = mysqli_connect('localhost', $usr, $pin, 'cs405');
 // Check connection
-if (mysqli_connect_errno($con))
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-  else
-  {
-  echo "Connection Established!";
-  }
+// if (mysqli_connect_errno($con))
+//   {
+//   echo "Failed to connect to MySQL: " . mysqli_connect_error();
+//   }
+//   else
+//   {
+//   echo "Connection Established!";
+//   }
 
 ?>
--- src/main.php
+++ src/main.php
@@ -33,13 +33,22 @@ if($count > 1){
 <TITLE> CS 405G Project </TITLE>
 </HEAD>
 <BODY>
+<div id="login" style="background-color:#FFFFFF;clear:both;text-align:right;">  
+<a href="customer_login.php">Login</a>
+<a href="customer_registration.php">Register</a>
+</div>
+
+<div id="footer" style="background-color:#FFFFFF;clear:both;text-align:center;">
 <H1> F&L Gift Store </H1>
-<br />
+</div>
+
+<div id="searchBox" style="background-color:#FFFFFF;clear:both;text-align:center;">
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method = "post">
 Search <input name="searchquery" type="text" size = "60" maxlength = "80">
 <input name = "myBtn" type = "submit" value = "GO!">
 </form>
-<div>
+</div>
+<div id="searchResult" style="background-color:#FFFFFF;clear:both;text-align:left;">
 <?php echo $search_output;?>
 </div>
 </BODY>
