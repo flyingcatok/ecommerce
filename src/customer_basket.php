ab5de3e3f8eaf18c71810acc73ce179f0ab7ddc3
Feiyu Shi <flyingcatok@gmail.com>
2013-11-09 15:53:51 -0500

    add search box to basket page

--- src/customer_basket.php
+++ src/customer_basket.php
@@ -13,9 +13,9 @@ $basket_output = "";
 					WHERE c.Email = b.CEmail AND b.CEmail = bc.CEmail AND b.BasketId = bc.BaskId
 							AND bc.IId = i.IId;";
 // connect to server
-include_once "connect_local.php";
+include "connect_local.php";
 $query = mysqli_query($con,$sqlCommand) or die(mysqli_error($con));
-include_once "disconnect.php";
+include "disconnect.php";
 $count = mysqli_num_rows($query);
 if($count >= 1){
 		$basket_output .= "<hr />$count items in your basket.<hr />";
@@ -36,6 +36,42 @@ if($count >= 1){
 
 ?>
 
+<?php
+// search function
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+$search_output = "";
+// process the search query
+if(isset($_POST['searchquery']) && $_POST['searchquery'] != ""){
+	$searchquery = preg_replace('#[^a-z 0-9?!]#i', '', $_POST['searchquery']);
+	//$sqlCommand = "SELECT * FROM Item WHERE MATCH('IName') AGAINST ('$searchquery')";
+	$sqlCommand = "SELECT * FROM Item WHERE IName LIKE '%$searchquery%' OR Category LIKE '%$searchquery%' OR Description LIKE '%$searchquery%'";
+// connect to server
+include "connect_local.php";
+$query = mysqli_query($con,$sqlCommand) or die(mysqli_error($con));
+include "disconnect.php";
+$count = mysqli_num_rows($query);
+if($count > 1){
+		$search_output .= "<hr />$count results for <strong>$searchquery</strong><hr />";
+		while($row = mysqli_fetch_array($query)){
+	            $id = $row["IId"];
+		    $name = $row["IName"];
+		    $category = $row["Category"];
+		    $descript = $row["Description"];
+		    $quantity = $row["Quantity"];
+		    $price = $row["IPrice"];
+		    $search_output .= "Item ID: $id - $name <br /> 
+		    Description: $descript <br />
+		    Category: $category <br />
+		    Quantity: $quantity<br /> 
+		    Unit Price: $price <br /><br />";
+                } // close while
+	} else {
+		$search_output = "<hr />0 results for <strong>$searchquery</strong><hr />";		
+}
+}
+?>
+
 <HTML>
 <HEAD>
 <TITLE> CS 405G Project </TITLE>
@@ -44,6 +80,18 @@ if($count >= 1){
 
 <div id="logo" style="background-color:#FFFFFF;clear:both;text-align:left;">
 <H2> F&L Gift Store </H2>
+</div>
+
+<div id="searchBox" style="background-color:#FFFFFF;clear:both;text-align:left;">
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method = "post">
+Search <input name="searchquery" type="text" size = "60" maxlength = "80">
+<input name = "myBtn" type = "submit" value = "GO!">
+</form>
+</div>
+
+<div id="search result" style="background-color:#FFFFFF;clear:both;text-align:left;">
+<?php echo $search_output;
+//output results in text?>
 </div>
 
 <div id="basket logo" style="background-color:#FFFFFF;clear:both;text-align:left;">
