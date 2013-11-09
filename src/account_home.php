46def0ac6a6de1a7b6d2c6de42651ae9c68bce2a
lafandahalf <Libby@Libbys-MacBook-Air.local>
2013-11-09 18:41:55 -0500

    Added customer login files including authentication.  Currently redirects to account_home no matter what and I'm not sure how to fix it.  Working on it.

---
+++ src/account_home.php
@@ -0,0 +1,51 @@
+<?php
+ //Author: Libby Ferland
+//Date: 11/9/2013
+//Last Edited: Libby Ferland
+//Last Edit Date: 11/9/2013
+    
+    include "connect_local.php";
+    include "header.php";
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
+    
+    //get variables from text input
+    
+    if(isset($_POST["loginBtn"])) {
+        if(isset($_POST["Email"])) {
+            $email = $_POST["Email"];
+        }
+        if(isset($_POST["Password"])) {
+            $pass = $_POST["Password"];
+        }
+    }
+    
+   // $email = $_POST['Email'];
+   // $pass = $_POST['Password'];
+    //$login = $_GET['loginBtn'];
+    
+    setcookie("username",$email, time()+1200);
+    
+        //query string and database connection
+        $findCustomer = "SELECT COUNT(*) FROM Customer WHERE Email='$email' AND Password='$pass'";
+        
+        
+        $getCustomerInfo = mysqli_query($con, $findCustomer);
+        $customerInfoResult = $getCustomerInfo->fetch_row();
+        
+    
+       // $resultCustomerInfo = mysqli_result($getCustomerInfo, 0);
+    
+        if ($customerInfoResult[0] !=1) {
+            echo "Invalid email or password";
+            echo "<HTML>";
+            echo "<br><br><a href=\"customer_login.php\">Return to login page</a><br>";
+            echo "<a href=\"customer_registration.php\">Create new account</a><br>";
+    
+    }
+        else {
+            echo "Login successful";
+        }
+    
+    
+?>
--- src/customer_login.php
+++ src/customer_login.php
@@ -1,3 +1,28 @@
-<?php
-echo "login: reserved for Libby.";
-?>
+
+<HTML>
+<HEAD>
+<TITLE> Store Login </TITLE>
+</HEAD>
+
+<BODY>
+<div id="goBack" style="background-color:#FFFFFF;clear:both;text-align:right;">
+<a href="main.php">Go Back to Main Screen</a>
+</div>
+
+<div id="login" style="background-color:#FFFFFF;clear:both;text-align:center;">
+<H1> Enter Login Information </H1>
+</div>
+<div id="loginInfo" style="background-color:#FFFFFF;clear:both;text-align:center;">
+<form action="account_home.php" method= "POST">
+Email: <input type="text" name="Email"><br>
+Password: <input type="password" name="Password"><br>
+<input type="submit" value="Login!" name="loginBtn">
+</form>
+</div>
+
+<div id="newuser" style="background-color:#FFFFFF;clear:both;text-align:left;">
+<a href="customer_registration.php">Create Account</a>
+</div>
+
+</BODY>
+</HTML>
