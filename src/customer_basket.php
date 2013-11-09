961374e01445e796f7141897dbac8a1bea122109
Feiyu Shi <flyingcatok@gmail.com>
2013-11-09 16:12:07 -0500

    separate search box in one file. update code in other pages which contains search box

--- src/customer_basket.php
+++ src/customer_basket.php
@@ -39,38 +39,7 @@ if($count >= 1){
 
 <?php
 // search function
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-$search_output = "";
-// process the search query
-if(isset($_POST['searchquery']) && $_POST['searchquery'] != ""){
-	$searchquery = preg_replace('#[^a-z 0-9?!]#i', '', $_POST['searchquery']);
-	//$sqlCommand = "SELECT * FROM Item WHERE MATCH('IName') AGAINST ('$searchquery')";
-	$sqlCommand = "SELECT * FROM Item WHERE IName LIKE '%$searchquery%' OR Category LIKE '%$searchquery%' OR Description LIKE '%$searchquery%'";
-// connect to server
-include "connect_local.php";
-$query = mysqli_query($con,$sqlCommand) or die(mysqli_error($con));
-include "disconnect.php";
-$count = mysqli_num_rows($query);
-if($count > 1){
-		$search_output .= "<hr />$count results for <strong>$searchquery</strong><hr />";
-		while($row = mysqli_fetch_array($query)){
-	            $id = $row["IId"];
-		    $name = $row["IName"];
-		    $category = $row["Category"];
-		    $descript = $row["Description"];
-		    $quantity = $row["Quantity"];
-		    $price = $row["IPrice"];
-		    $search_output .= "Item ID: $id - $name <br /> 
-		    Description: $descript <br />
-		    Category: $category <br />
-		    Quantity: $quantity<br /> 
-		    Unit Price: $price <br /><br />";
-                } // close while
-	} else {
-		$search_output = "<hr />0 results for <strong>$searchquery</strong><hr />";		
-}
-}
+include "search.php";
 ?>
 
 <HTML>
--- src/main.php
+++ src/main.php
@@ -4,38 +4,7 @@
 //Last Edited: 
 //Date:
 
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-$search_output = "";
-// process the search query
-if(isset($_POST['searchquery']) && $_POST['searchquery'] != ""){
-	$searchquery = preg_replace('#[^a-z 0-9?!]#i', '', $_POST['searchquery']);
-	//$sqlCommand = "SELECT * FROM Item WHERE MATCH('IName') AGAINST ('$searchquery')";
-	$sqlCommand = "SELECT * FROM Item WHERE IName LIKE '%$searchquery%' OR Category LIKE '%$searchquery%' OR Description LIKE '%$searchquery%'";
-// connect to server
-include_once "connect_local.php";
-$query = mysqli_query($con,$sqlCommand) or die(mysqli_error($con));
-include_once "disconnect.php";
-$count = mysqli_num_rows($query);
-if($count > 1){
-		$search_output .= "<hr />$count results for <strong>$searchquery</strong><hr />";
-		while($row = mysqli_fetch_array($query)){
-	            $id = $row["IId"];
-		    $name = $row["IName"];
-		    $category = $row["Category"];
-		    $descript = $row["Description"];
-		    $quantity = $row["Quantity"];
-		    $price = $row["IPrice"];
-		    $search_output .= "Item ID: $id - $name <br /> 
-		    Description: $descript <br />
-		    Category: $category <br />
-		    Quantity: $quantity<br /> 
-		    Unit Price: $price <br /><br />";
-                } // close while
-	} else {
-		$search_output = "<hr />0 results for <strong>$searchquery</strong><hr />";		
-}
-}
+include "search.php";
 ?>
 <HTML>
 <HEAD>
@@ -58,6 +27,7 @@ Search <input name="searchquery" type="text" size = "60" maxlength = "80">
 <input name = "myBtn" type = "submit" value = "GO!">
 </form>
 </div>
+
 <div id="searchResult" style="background-color:#FFFFFF;clear:both;text-align:left;">
 <?php echo $search_output;
 //output results in text?>
