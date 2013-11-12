d459c0d384c2fa06354621eb2ab544bbf421d9a3
Feiyu Shi <flyingcatok@gmail.com>
2013-11-12 15:53:04 -0500

    add a link to logo 'F&L Gift Store'. need to include this one on almost every page.

--- src/customer_order_history.php
+++ src/customer_order_history.php
@@ -53,7 +53,7 @@ include "search.php";
 <BODY>
 
 <div id="logo" style="font-color:#FFF234;clear:both;text-align:left;">
-<H2> F&L Gift Store </H2>
+<H2> <a href="main.php" style="text-decoration: none">F&L Gift Store</a> </H2>
 </div>
 
 <div id="searchBox" style="background-color:#FFFFFF;clear:both;text-align:left;">
