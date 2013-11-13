2776298a44a7863c25244de4d5d3bd364ac151d6
Feiyu Shi <flyingcatok@gmail.com>
2013-11-12 19:09:03 -0500

    update memo;add 'add to basket' button to item pages;add functionality 'add the item to the basket'

--- src/items/iid=111.php
+++ src/items/iid=111.php
@@ -1,12 +1,14 @@
 <?php
 //Author: Feiyu Shi
 //Date: 11/9/2013
-//Last Edited: 
-//Date:
+//Last Edited: Feiyu Shi
+//Date: 11/12/2013
 
 // display attributes of the item
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
+ini_set('display_startup_errors','1');
+
 $item_output = "";
 // process the query
 	$sqlCommand = "SELECT i.IId, i.IName, i.Category, i.Description, i.Quantity, i.IPrice
@@ -45,7 +47,7 @@ include "../search.php";
 <BODY>
 
 <div id="logo" style="background-color:#FFFFFF;clear:both;text-align:left;">
-<H2> F&L Gift Store </H2>
+<H2> <a href="main.php" style="text-decoration: none">F&L Gift Store</a> </H2>
 </div>
 
 <div id="searchBox" style="background-color:#FFFFFF;clear:both;text-align:left;">
@@ -67,8 +69,23 @@ Search <input name="searchquery" type="text" size = "60" maxlength = "80">
 <img src="imgs/51eYy1tSgnL._SX385_.jpg" alt="lego minecraft" style="width:auto; max-height:500px;">
 </div>
 
-<div id="content" style="background-color:#FFFFFF;height:500px;width:500px;float:right;">
+<div id="content" style="background-color:#FFFFFF;height:300px;width:500px;float:right;">
 <?php echo $item_output ?>
+</div>
+
+<div id="quantity dropdown" style="background-color:#FFFFFF;height:200px;width:500px;float:right;">
+<form action="../add_item_to_basket.php" method = "post">
+<label for="iquantity">Quantity:</label>
+<select name="iquantity" id="iquantity">
+  <option value="1" selected>1</option>
+  <option value="2">2</option>
+  <option value="3">3</option>
+  <option value="4">4</option>
+  <option value="5">5</option>
+</select>
+<input type="hidden" name="IID" value="111">
+<input type="submit" value="Add to Basket">
+</form>
 </div>
 
 </div>
