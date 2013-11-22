3441ad0648d0a2f330332ab9161faee7f84546be
Feiyu Shi <flyingcatok@gmail.com>
2013-11-22 14:48:06 -0500

    change styles of address and payment pages

--- src/my_account.php
+++ src/my_account.php
@@ -52,9 +52,9 @@
     </div>
     <div id ="navigation" style ="background-color: #FFFFFF; clear:both;height:300px;width:300px;float:left">
         <h4>Manage My Account</h4>
-        <a href ="customer_order_history.php">My Orders</a><br>
+        <a href ="customer_order_history.php">My Orders</a><br/>
         <a href ="my_payment.php">My Payment Info</a><br>
-        <a href ="my_address.php">My Address</a><br> 
+        <a href ="my_address.php">My Address Book</a><br> 
 <!--         <a href ="customer_basket.php">My Basket</a><br> -->
         <a href ="main.php">Go Shopping!</a><br><br>
         
