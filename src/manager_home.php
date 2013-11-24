0ef6ed4d8080b87560a08c9505b92ec92d8b2343
Feiyu Shi <flyingcatok@gmail.com>
2013-11-24 11:31:46 -0500

    add remove promotion button to manager page; also want to add change rate and extend promotion button, failed;may be picked up some time later

--- src/manager_home.php
+++ src/manager_home.php
@@ -2,7 +2,7 @@
 //Author: Feiyu Shi
 //Date: 11/16/2013
 //Last Edit: Feiyu Shi
-//Edit Date: 11/23/2013
+//Edit Date: 11/24/2013
 
     error_reporting(E_ALL);
     ini_set('display_errors', '1');
@@ -10,10 +10,22 @@
     session_start();
     
     if(isset($_SESSION['empID'])) {
-        $homeEID = $_SESSION['empID'];
+        $manaEID = $_SESSION['empID'];
 //         echo $homeEID;
-    }
     
+    include "connect_local.php";
+    
+    $findEmployee = "SELECT * FROM Employee WHERE EId = '$manaEID'";
+    
+    $getEmployee = mysqli_query($con, $findEmployee);
+    
+    $employeeInfo = $getEmployee->fetch_row();
+    
+    $empFirst = $employeeInfo[3];
+    $empPrivileges = $employeeInfo[4];
+    
+    include "disconnect.php";
+    }
 ?>
 
 <HTML>
@@ -32,6 +44,7 @@
     <h2>Sales Statistics</h2>
     <p>as of<?php  $today = date_create("",timezone_open("America/New_York")); echo " ".date_format($today,"Y-m-d H:i:s");?> </p>
     <hr />
+   <?php if(isset($_SESSION['empID'])&&$empPrivileges==1){?>
     <p>View by date:</p>
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method = "post">
 	<input name="period" type="radio" value="Today">Today's sales history<br />
@@ -43,6 +56,7 @@
 	</form>
 	<?php include "manager_statistics.php";?>
 	<hr />
+	<?php }?>
     </div>
     
     <div>
@@ -51,6 +65,7 @@
     <p>as of<?php  $today = date_create("",timezone_open("America/New_York")); echo " ".date_format($today,"Y-m-d H:i:s");?> </p>
     <hr />
     <h4>Current Promotions:</h4>
+    <?php if(isset($_SESSION['empID'])&&$empPrivileges==1){?>
     <div>
     <?php 
     error_reporting(E_ALL);
@@ -59,17 +74,17 @@
     // connect to server
 	include "connect_local.php";
 	//update new promotion rate
-	if(isset($_POST['IID'])&&isset($_POST['promorate'])&& $_POST['promorate']!=""&&$_POST['promorate']>0&&$_POST['promorate']<100&&is_numeric($_POST['promorate'])){
-		$promorate = $_POST['promorate']*0.01;
-		$iid = $_POST['IID'];
-		$sqlrate = "UPDATE Promote SET PromoteRate = $promorate WHERE IId = $iid";
-		$rateupdate = mysqli_query($con,$sqlrate) or die(mysqli_error($con));
-		// //update the item promo price
+	// if(isset($_POST['IID'])&&isset($_POST['promorate'])&& $_POST['promorate']!=""&&$_POST['promorate']>0&&$_POST['promorate']<100&&is_numeric($_POST['promorate'])){
+// 		$promorate = $_POST['promorate']*0.01;
+// 		$iid = $_POST['IID'];
+// 		$sqlrate = "UPDATE Promote SET PromoteRate = $promorate WHERE IId = $iid";
+// 		$rateupdate = mysqli_query($con,$sqlrate) or die(mysqli_error($con));
+// 		//update the item promo price
 // 		$sqlupdate = "UPDATE Item i, Promote p
-// 					SET i.PromoPrice = i.IPrice * (1-p.PromoteRate)
-// 					WHERE p.IId = i.IId AND p.IId = $iid";
+// 					SET i.PromoPrice = i.IPrice * (1-$promorate)
+// 					WHERE p.IId = i.IId AND p.IId = $iid AND p.PStartDate <= CURDATE()";
 // 		$queryupdate = mysqli_query($con,$sqlupdate) or die(mysqli_error($con));
-	}
+// 	}
 	//extend a promotion
 	// function checkDateTime($data) {
 //     if (date('Y-m-d', strtotime($data)) == $data) {
@@ -99,10 +114,11 @@
 			
 	}
 
-	// set the promotion price when the promotion start date is today
+	// set the promotion price when the promotion start date is today or update promo rate when the 'set' is clicked and it's already on sale
+	
 	$sqlupdate = "UPDATE Item i, Promote p
 					SET i.PromoPrice = i.IPrice * (1-p.PromoteRate)
-					WHERE p.IId = i.IId AND p.PStartDate = TIMESTAMP(CURDATE(),'00:00:00')";
+					WHERE p.IId = i.IId AND p.PStartDate = CURDATE()";
 	$queryupdate = mysqli_query($con,$sqlupdate) or die(mysqli_error($con));
 	
 	// delete the promotion that expired yestoday and update the item price
@@ -119,7 +135,8 @@
 	// display current promotion
 	$sqlpromo = "SELECT p.IId, i.IName, i.IPrice, p.PromoteRate, i.PromoPrice AS CurrPrice, p.PStartDate, p.PEndDate
 					FROM Promote p,Item i
-					WHERE p.IId = i.IId AND p.PEndDate >= CURDATE()";
+					WHERE p.IId = i.IId AND p.PEndDate >= CURDATE()
+					ORDER BY p.PStartDate";
 	$querypromo = mysqli_query($con,$sqlpromo) or die(mysqli_error($con));
 	$count = mysqli_num_rows($querypromo);
 	if($count > 0){
@@ -147,12 +164,14 @@
 		    echo ("<tr><td>$id</td>");
 			echo ("<td>$name</td>");
 			echo ("<td>$price</td>");
-// 			echo ("<td>$rate% off</td>");?>
-			<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method = "post">
+			echo ("<td>$rate% off</td>");?>
+			<!-- 
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method = "post">
 			<?php echo "<td><input type='text' name=promorate value = $rate size = '3'>% off" ?>
 			<?php echo "<input type ='hidden' name=IID value = $id>";?>
 			<input type="submit" value="set"></td>
 			</form>
+ -->
 			<?php
 			echo ("<td>$currprice</td>");
 			echo ("<td>$startdate</td>");
@@ -195,5 +214,5 @@
 
     </div>
     </div>
-    
+    <?php }?>
 </HTML>
